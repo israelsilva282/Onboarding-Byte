0dd3e1ed757b96ad983c00b71d085e7a21359cc1
israelsilva282 <israelcostas@hotmail.com>
2023-04-19 09:36:46 -0300

    feat: Criação da logo (#1)

---
+++ src/components/MyLogo.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mainLogo">
+    <div class="logo1"></div>
+    <div class="logo2"></div>
+    <div class="logo3"></div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.mainLogo {
+  height: 263px;
+  width: 197px;
+}
+
+.logo1 {
+  background: #fd59b7;
+  position: absolute;
+  height: 80px;
+  width: 196px;
+}
+
+.logo2 {
+  background: #f43ea7;
+  position: absolute;
+  height: 263px;
+  width: 93.39px;
+  border-radius: 0px;
+}
+
+.logo3 {
+  background: #fd59b7;
+  position: absolute;
+  height: 83px;
+  width: 156px;
+  margin-top: 111px;
+}
+</style>
