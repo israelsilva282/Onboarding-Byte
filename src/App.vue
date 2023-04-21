b37fffc0cf5f603aefc76dc6358fc8ca777a4020
israelsilva282 <israelcostas@hotmail.com>
2023-04-21 09:25:55 -0300

    style: adicionado font-family Rubik (#2)

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,16 @@ export default {};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
 
-body,
-html {
+body {
   min-height: 100vh;
+  font-family: "Rubik", sans-serif;
   background-color: #13111b;
 }
 
