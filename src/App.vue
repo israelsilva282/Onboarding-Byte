ed728660ec20879ea3507fa36adc7489a23c15c9
israelsilva282 <israelcostas@hotmail.com>
2023-04-21 10:42:04 -0300

    feat: adicionado transição de tela inicial para forum apos 2 segundos

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,18 @@
 </template>
 
 <script>
-export default {};
-</script>
+export default {
+  methods: {
+    changePage() {
+      setTimeout(() => this.$router.push("/forum"), 2000);
+    },
+  },
 
+  mounted() {
+    this.changePage();
+  },
+};
+</script>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
 * {
